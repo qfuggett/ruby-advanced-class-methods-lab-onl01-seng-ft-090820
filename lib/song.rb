8c5edadc192828408c9d2cd57b9834b2866ef67c
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 20:48:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   
   def self.new_from_filename(filename)
     binding.pry
-    no_filename = filename.chomp(".mp3")        
+    no_fileformat = filename.chomp(".mp3")        
     split_song_artist = [ ]
         split_up = filename.split(" - ")
         split_song_artist << split_up
