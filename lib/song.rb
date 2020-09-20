5b5523791ad67a4b288a8eb3c958867773cfada7
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 20:04:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)    #if find_by_name returns true it stays left, else right
   end
   
-  def self.alphabetical(name)
+  def self.alphabetical
     binding.pry
   end
   
